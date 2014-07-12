9e95d2a1d08d6f8991860426dfee01d9d8c40ffd
Jack Cox <jcox@captechventures.com>
2014-07-12 16:46:20 -0400

    Initial Commit

---
+++ mastering Interface builderTests/mastering_Interface_builderTests.swift
@@ -0,0 +1,36 @@
+//
+//  mastering_Interface_builderTests.swift
+//  mastering Interface builderTests
+//
+//  Created by Jack Cox on 7/12/14.
+//  Copyright (c) 2014 CapTech Consulting. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class mastering_Interface_builderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
