e41c44c9c66aa5b5ace74854ff32aef8b8a3d02a
Jack Cox <jcox@captechconsulting.com>
2016-03-16 20:05:12 -0400

    initial checkin

--- mastering Interface builder/ViewController.swift
+++ mastering Interface builder/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
                             
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var titleHeightConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +20,27 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 14
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("forecastCell", forIndexPath: indexPath) as! WeatherCellCollectionViewCell
+        
+        cell.backgroundColor = UIColor.yellowColor()
+        cell.label.text = "Today"
+        // Configure the cell
+        
+        return cell
     }
 
 
---
+++ mastering Interface builder/WeatherCellCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCellCollectionViewCell.swift
+//  mastering Interface builder
+//
+//  Created by Jack Cox on 3/16/16.
+//  Copyright © 2016 CapTech Consulting. All rights reserved.
+//
+
+import UIKit
+
+class WeatherCellCollectionViewCell: UICollectionViewCell {
+    @IBOutlet
+    var label:UILabel!
+    
+}
